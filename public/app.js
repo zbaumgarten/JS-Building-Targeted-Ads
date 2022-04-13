43fa5c6d3ec5c1a8ab0e0c453c9e36612b875541
Zachary Baumgarten <zach.baumgarten@yahoo.com>
2022-04-12 20:43:08 -0500

    commitement!

--- public/app.js
+++ public/app.js
@@ -1,8 +1,25 @@
-// get user's data
-// get user's coordinates
+// get user's data                                                            
+// Get the user's coordinates:                                                              
+async function getCoords(){
+    pos = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject)
+    })
+    return [pos.coords.latitude, pos.coords.longitude]
+}
 
+console.log(getCoords());                            
 
-// get user's time
+// Get the user's time:                                                              
+function userTime(){
+    const now = new Date()
+    return now.getHours()
+}
+console.log(userTime())    
+
+function getMealTime(){
+    const tod = userTime()
+    return tod > 20 ? 'latenight snack' : tod > 16 ? 'dinner' : tod > 11 ? 'lunch' : 'breakfast'
+}                        
 
 
 // helper functions
@@ -11,10 +28,30 @@
 
 // build ads
 // build ad 1
+function buildAd1(){
+    const mealTime = getMealTime()
+    let content = document.querySelector('.ad1')
+    let inner = document.createElement('p')
+    inner.innerHTML = `We've got the best <span>${mealTime}</span> in town`
+    content.append(inner)
+}
 
 
-// build ad 2
+// build ad 2                                                         
+function buildAd2(coordinates){
+    const coords = coordinates
+    const href = `https://www.google.com/maps/search/coffee/@${coords[0]},${coords[1]},15z/`
+    let content = document.querySelector('.ad2')
+    let inner = document.createElement('p')
+    inner.innerHTML = `It's time to try our coffee! <span><a href="${href}" target="_blank">We're this close!</a></span>`
+    content.append(inner)
+}
 
 
 // event listeners
-// on load, build ads
+// On load, build ads:                                                             
+window.onload = async () => {
+    buildAd1()
+    const coords = await getCoords()
+    buildAd2(coords)
+}
